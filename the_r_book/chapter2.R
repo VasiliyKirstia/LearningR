65dd54c24838b7ab58c29e2a9167961694b15c74
Vasiliy Kirstia <vasiliykirstia@yandex.ru>
2016-12-13 19:10:20 +0200

    add old rows from notebook

--- the_r_book/chapter2.R
+++ the_r_book/chapter2.R
@@ -1,3 +1,112 @@
+x <- sqrt(2)
+identical(x^2, 2)
+all.equal(x^2, 2)
+rm(list = ls())
+
+x <- c(1, 2, 3)
+y <- c(1, 2, 3)
+x %o% y
+x %*% y
+rm(list = ls())
+
+x <- matrix(c(1, 2, 3, 4), nrow = 2)
+y <- matrix(c(1, 2, 3, 4), ncol = 2)
+x %o% y
+x %*% y
+rm(list = ls())
+
+x <- c(NA, TRUE, FALSE)
+names(x) <- c("NA", "TRUE", "FALSE")
+outer(x, x, "&")
+outer(x, x, "|")
+rm(list = ls())
+
+(t5 <- letters[1:5])
+factor(1 + (t5 == "b") + (t5 == "c")*2 + (t5 == "d")*2)
+
+seq(from=0, by=10, along= c(3,3,4,5))
+seq(from=0, by=10, to=50)
+seq(from=0, by=10, length=3)
+
+rep(1:5, c(1,5,0,0,2))
+rep(1:5, times=2)
+rep(1:5, each=2)
+rep(1:5, each=2, times=2)
+rep(c("cats", "pig", "dog"), each=2, times=2)
+rep(c(FALSE, NA), each=2, times=2)
+
+gl(2,3,14, labels = c("F", "C"))
+gl(5,1,20, labels = c("A", "B", "C", "D", "F"))
+gl(5,1,20, labels = c("A", "B", "C", "D"))
+
+which(rep(c(TRUE, FALSE), times=3, each=2))
+
+x <- rnorm(100, mean = 70, sd=15)
+mean(x)
+sum(x < mean(x))
+rm(list = ls())
+
+x <- rnorm(10000000, 0, 100)
+sum(rev(sort(x))[1:3])
+which(x == max(x))
+which.max(x)
+which(x == pmax(x))
+
+range(x)
+median(x)
+quantile(x)
+fivenum(x)
+var(x)
+rank(x)
+cummin(x)
+table(x)
+rm(list = ls())
+
+dummy_data = data.frame(
+  temp = rnorm(10000, 10, 50),
+  weat = runif(10000,740, 790),
+  month = sample(month.name, 10000, replace = TRUE),
+  year = sample(2000:2016, 10000, replace = TRUE))
+with(dummy_data, tapply(temp, month, mean))
+with(dummy_data, tapply(temp, list(year, month), mean)[,1:6])
+with(dummy_data, tapply(temp, year, mean, trim=0.1))
+
+aggregate(temp ~ month, dummy_data, mean)
+aggregate(temp ~ month + year, dummy_data, mean)
+aggregate(temp + weat ~ month, dummy_data, mean)
+aggregate(cbind(temp,weat) ~ month, dummy_data, mean)
+aggregate(cbind(temp,weat) ~ month + year, dummy_data, mean)
+
+aggregate(temp ~ month:year, dummy_data, mean)
+aggregate(temp ~ month*year, dummy_data, mean)
+rm(list=ls())
+
+xv <- rnorm(10000, mean=100, sd=100)
+closest <- function(x, value){
+  x[which(abs(x-value) == min(abs(x-value)))]
+}
+closest(xv, 108)
+rm(list = ls())
+
+x <- data.frame(
+  weight = sample(50:100, 50, replace = TRUE), 
+  gender=sample(c("M", "F"), 50, replace = TRUE))
+x$gender[order(x$weight)]
+x
+sort(x$weight)
+x
+
+duplicated(x$gender)
+unique(x$gender)
+mean( x$weight[!duplicated(x$gender)] )
+rm(list=ls())
+
+x <- rpois(1000, 0.7)
+r_l_e <- rle(x)
+max(r_l_e$lengths)
+r_l_e$values[which(r_l_e$lengths == max(r_l_e$lengths))]
+rm(list = ls())
+
 setA <- c("a", "b", "c", "d")
 setB <- c("c", "d", "e", "f")
 intersect(setA, setB)
