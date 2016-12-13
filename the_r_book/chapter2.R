2a0f07a9020b9d0b116cc07b50d01e8bdd9222e7
VasiliyKirstia <vailiykirstia@yandex.ru>
2016-12-13 10:57:39 +0200

    creating and manitulstions with matrixes

--- the_r_book/chapter2.R
+++ the_r_book/chapter2.R
@@ -7,3 +7,25 @@ setdiff(setB, setA)
 setA %in% setB
 setA %in% setA
 setA[setA %in% setB]
+rm(list = ls())
+
+
+(v <- 1:24)
+dim(v)
+dim(v) <- c(3,4,2)
+v
+dim(v) <- c(2,3,4)
+v
+dim(v) <- c(24,1)
+v
+(v <- as.numeric(v))
+m <- matrix(v, nrow = 6, byrow = TRUE)
+matrix(v, nrow = 4)
+matrix(v, nrow = 2, ncol = 3, byrow = TRUE)
+m[,3]
+m[3,]
+m[3] #непонятная фигня
+class(m)
+attributes(m)
+t(m)
+rm(list = ls())
