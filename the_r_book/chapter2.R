f5a90d4f4b04835e3999d88aff55bc3921d4ea82
Vasiliy Kirstia <vailiykirstia@yandex.ru>
2016-12-13 10:24:06 +0200

    sets and operations under sets

--- the_r_book/chapter2.R
+++ the_r_book/chapter2.R
@@ -1,1 +1,9 @@
-print("Hello world!")
+setA <- c("a", "b", "c", "d")
+setB <- c("c", "d", "e", "f")
+intersect(setA, setB)
+union(setA, setB)
+setdiff(setA, setB)
+setdiff(setB, setA)
+setA %in% setB
+setA %in% setA
+setA[setA %in% setB]
