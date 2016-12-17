e66ff17c1956baa1c7b36c5a88a57e4bcf8cafc0
Vasiliy Kirstia <vasiliykirstia@yandex.ru>
2016-12-17 15:56:43 +0200

    scan

--- the_r_book/chapter3.R
+++ the_r_book/chapter3.R
@@ -10,3 +10,6 @@ dir()
 data <- read.table(file.choose(), header = TRUE)
 data <- read.table(file.choose(), header = TRUE, as.is = TRUE) #without converting to factors
 #data <- read.table("http://web.site.ru/data.csv", header = TRUE) get data from web
+
+data <- scan(file.choose(), skip = 1, what = as.list(c(character(1), numeric(1), numeric(1))))
+str(data)
