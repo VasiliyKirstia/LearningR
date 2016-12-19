e68180375c28ac38c5416bca6c8ee5b3d2e826e5
Vasiliy Kirstia <kirstiavasilii@gmail.com>
2016-12-19 10:27:18 +0200

    subscripts

--- the_r_book/chapter4.R
+++ the_r_book/chapter4.R
@@ -5,3 +5,18 @@ iris
 summary(iris)
 by(iris, iris$Species, summary)
 by(iris, iris$Species, function(x) lm(Sepal.Length ~ Sepal.Width, data = x))
+
+#subscripts
+iris[2,3]
+iris[1:15,3:4]
+class(iris[2,])
+class(iris[,2])
+iris[iris$Species=="virginica",]
+iris[iris$Species=="virginica",c(1,3)]
+
+iris[sample(1:nrow(iris), 5),]
+iris[order(iris$Petal.Length),]
+iris[rev(order(iris$Petal.Length)),]
+iris[order(iris$Species, iris$Petal.Length),]
+iris[order(iris$Species, iris$Petal.Length, iris$Petal.Width), 
+     c("Species", "Petal.Length", "Petal.Width")]
